c17441a1d14c3114f5b20b6de90781e1bf8d6b81
trybe-tech-ops <82593112+trybe-tech-ops@users.noreply.github.com>
2023-03-20 14:19:59 -0300

    Initial commit

---
+++ src/main/java/com/trybe/acc/java/numerospares/NumerosPares.java
@@ -0,0 +1,25 @@
+package com.trybe.acc.java.numerospares;
+
+/**
+ * Classe NumerosPares.
+ * 
+ */
+public class NumerosPares {
+
+  /**
+   * Método para implementar o for que vai gerar os números de 4 a 20.
+   * 
+   */
+  public static void main(String[] args) {
+
+  }
+
+  /**
+   * Método para imprimir no console os números de 4 a 20.
+   * 
+   */
+  public static void imprime(int numero) {
+    System.out.println(numero);
+  }
+
+}
